3e2369f23138fcb2a929a16451498c2c414f51d9
theoluwafemi <oluwafemidamilola21@gmail.com>
2022-03-05 17:05:18 +0100

    fix rroute guard issue

--- src/router/index.js
+++ src/router/index.js
@@ -6,19 +6,13 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
+      path: "",
       name: "home",
       component: HomeView,
-      meta: { requiresAuth: true },
-    },
-    {
-      path: "",
-      redirect: { name: "home" },
     },
     {
       path: "/auth",
       component: () => import("@/views/authentication/AuthenticationView.vue"),
-      meta: { requiresAuth: false },
       children: [
         {
           path: "",
@@ -26,12 +20,12 @@ const router = createRouter({
         },
         {
           path: "sign-in",
-          name: "sign-in",
+          name: "auth-sign-in",
           component: () => import("@/views/authentication/SignIn.vue"),
         },
         {
           path: "sign-up",
-          name: "sign-up",
+          name: "auth-sign-up",
           component: () => import("@/views/authentication/SignUp.vue"),
         },
       ],
@@ -41,12 +35,10 @@ const router = createRouter({
 
 router.beforeEach((to) => {
   const authStore = useAuthStore();
-  if (!authStore.userSignedIn && to.meta.requiresAuth) {
-    return {
-      path: "/auth/sign-in",
-    };
-  } else if (authStore.userSignedIn && !to.meta.requiresAuth) {
-    return { path: to.path };
+  if (!authStore.userSignedIn && !to.name.includes("auth")) {
+    return { name: "auth-sign-in" };
+  } else if (to.name.includes("auth") && authStore.userSignedIn) {
+    return { name: "home" };
   }
 });
 
